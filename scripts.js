9ac28295aa174cfc3fee46a5122fe282bb5dbef3
Armando Galván <armandocgv@gmail.com>
2016-12-25 22:15:51 -0600

    Custom styled

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-window.addEventListener('keydown', function(e){
+function playSound(e) {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
@@ -6,4 +6,14 @@ window.addEventListener('keydown', function(e){
 	audio.currentTime = 0;
 	audio.play();
 	key.classList.add('playing');
-});
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove("playing");
+ }
+
+const keys = document.querySelectorAll('.key');
+console.log(keys);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
